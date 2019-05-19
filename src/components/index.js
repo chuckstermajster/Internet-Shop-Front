cf24bf1b958f5db32324dea345e89c6e36182473
Mateusz <chuckstermajster@gmail.com>
2019-05-19 19:57:25 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Test from "./test";
+
+class MainComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <Test />
+        <h2>Test komponentu klasowego</h2>
+      </div>
+    );
+  }
+}
+export default MainComponent;
